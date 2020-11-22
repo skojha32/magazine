97dce536618e3b3833980537038587b4d5214437
lolboy1337 <31764504+lolboy1337@users.noreply.github.com>
2020-11-22 19:01:35 +0530

    Update report.php

--- report.php
+++ report.php
@@ -286,7 +286,6 @@
 <br>
 <!-- ##### Portfolio Area End ###### -->
 
-<!-- ##### Footer Area Start ##### -->
 <footer class="footer-area section-padding-30-0">
 
 
@@ -294,10 +293,10 @@
   <div class="copywrite-area">
     <div class="container-fluid">
       <div class="row align-items-center">
-        <div class="col-12 col-md-6">
-          <div class="copywrite-content">
+        <div class="col-12 col-md-12">
+          <div class="copywrite-content" >
             <!-- Copywrite Text -->
-            <p class="copywrite-text"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            <p class="copywrite-text" style="margin-left:30%;"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
               Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
               <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
             </p>
